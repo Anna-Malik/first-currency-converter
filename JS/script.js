728936f19d706e2ff7fb27af9ad36f505d496a7d
Anna Malik <malik.anna1992@gmail.com>
2022-03-15 09:54:33 +0100

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+let amountElement = document.querySelector(".js-amount");
+let rateElement = document.querySelector(".js-rate");
+let formElement = document.querySelector(".js-form");
+let resultElement = document.querySelector(".js-result");
+
+formElement.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    let amount = amountElement.value;
+    let rate = rateElement.value;
+    let result = amount * rate;
+
+    resultElement.innerText = result.toFixed(2);
+
+})
