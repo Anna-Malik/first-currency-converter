cbbb555d3fc9b972343457a0b66df8c69b57f5e3
Anna Malik <malik.anna1992@gmail.com>
2022-03-18 09:22:09 +0100

    JS switch added, correct BEM added

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,30 @@
 let amountElement = document.querySelector(".js-amount");
-let rateElement = document.querySelector(".js-rate");
+let currencyElement = document.querySelector(".js-currency");
 let formElement = document.querySelector(".js-form");
 let resultElement = document.querySelector(".js-result");
+
+let rateUSD = 4.228;
+let rateEUR = 4.685;
 
 formElement.addEventListener("submit", (event) => {
 
     event.preventDefault();
 
-    let amount = amountElement.value;
-    let rate = rateElement.value;
-    let result = amount * rate;
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+    
+    let result;
 
-    resultElement.innerText = result.toFixed(2);
+switch (currency) {
+
+    case "USD":
+        result = amount*rateUSD;
+        break;
+
+    case "EUR":
+        result = amount*rateEUR;
+        break;
+}
+    resultElement.innerText = `${result.toFixed(2)}`
 
 })
